fb2f8ffa53989a369dd36c2afd74b22608e5afd3
Fabio Coimbra <fabiogc1989@gmail.com>
2021-11-01 10:11:08 +0000

    File Utility test in main method

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,12 +5,8 @@
 package application;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-import javax.mail.MessagingException;
-import model.models.MailFile;
-import model.services.MailService;
+import java.util.List;
+import util.FileUtility;
 
 /**
  *
@@ -20,24 +16,28 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Properties props = System.getProperties();
-        props.put("mail.smtp.Host", "smtp-mail.Outlook.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.starttls.enable", "true");
+//        Properties props = System.getProperties();
+//        props.put("mail.smtp.Host", "smtp-mail.Outlook.com");
+//        props.put("mail.smtp.port", "587");
+//        props.put("mail.smtp.starttls.enable", "true");
+//
+//        try {
+//            File emlFile = new File("C:\\temp\\test.eml");
+//            
+//            MailService mailService = new MailService(props, null);
+//            MailFile mailFile = mailService.openMessage(emlFile);
+//            mailFile.saveAllAttachments("C:\\temp");
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("Error: " + ex.getMessage());
+//        } catch (IOException ex) {
+//            System.out.println("Error: " + ex.getMessage());
+//        } catch (MessagingException ex) {
+//            System.out.println("Error: " + ex.getMessage());
+//        }
 
-        try {
-            File emlFile = new File("C:\\temp\\test.eml");
-            
-            MailService mailService = new MailService(props, null);
-            MailFile mailFile = mailService.openMessage(emlFile);
-            mailFile.saveAllAttachments("C:\\temp");
-        } catch (FileNotFoundException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        } catch (MessagingException ex) {
-            System.out.println("Error: " + ex.getMessage());
+        List<File> myFiles = FileUtility.getFiles("C:\\temp\\folder1");
+        for(File item : myFiles){
+            System.out.println(item.getAbsoluteFile());
         }
-
     }
 }
